a54e577947024b3f0f14b1e82914aba8e2ec1b50
burbridw <111381782+burbridw@users.noreply.github.com>
2022-12-06 17:01:59 +0900

    game modes introduced

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const roomsGrid = document.querySelector(".rooms-grid")
 const moverGrid = document.querySelector(".mover-grid")
 const entrance = document.querySelector(".entrance")
 
+let gameActive = false
+let secret = false
 let moverPosition = 38
 
 const allRoomsArr = ["./images/school/img1.png","./images/school/img2.png", "./images/school/img3.png", "./images/school/img4.png", "./images/school/img5.png", "./images/school/img6.png", "./images/school/img7.png", "./images/school/img8.png", "./images/school/img9.png", "./images/school/img10.png", "./images/school/img11.png", "./images/school/img12.png", "./images/school/img13.png", "./images/school/img14.png", "./images/school/img15.png", "./images/school/img16.png"]
+const numbersArr = ["./images/numbers/img1.png","./images/numbers/img2.png", "./images/numbers/img3.png", "./images/numbers/img4.png", "./images/numbers/img5.png", "./images/numbers/img6.png", "./images/numbers/img7.png", "./images/numbers/img8.png", "./images/numbers/img9.png", "./images/numbers/img10.png", "./images/numbers/img11.png", "./images/numbers/img12.png"]
 
 const roomsObj = {
     "classroom": "./images/school/img1.png",
@@ -28,43 +31,143 @@ const moverObj = {
     "policecar": "./images/movers/policecar.png",
     "fighterjet": "./images/movers/fighterjet.png",
 }
-start()
 
-function start() {
+const entryObj = {
+    0: 0, 2: 1, 4: 2, 6: 3, 14: 4, 16: 5, 18: 6, 20: 7, 28: 8, 30: 9, 32: 10, 34: 11
+}
+
+let theMover = moverObj["elephant"]
+
+const practiceBtn = document.querySelector(".practice")
+const treasureBtn = document.querySelector(".treasure")
+const secretBtn = document.querySelector(".secret")
+
+practiceBtn.addEventListener("click",()=>{
+    if ( !practiceBtn.classList.contains("toggleon") ) {
+        document.querySelector(".toggleon").classList.remove("toggleon")
+        practiceBtn.classList.add("toggleon")
+    }
+    startPractice()
+})
+treasureBtn.addEventListener("click",()=>{
+    if ( !treasureBtn.classList.contains("toggleon") ) {
+        document.querySelector(".toggleon").classList.remove("toggleon")
+        treasureBtn.classList.add("toggleon")
+    }
+    startTreasure()
+})
+secretBtn.addEventListener("click",()=>{
+    if ( !secretBtn.classList.contains("toggleon") ) {
+        document.querySelector(".toggleon").classList.remove("toggleon")
+        secretBtn.classList.add("toggleon")
+    }
+    startSecret()
+})
+
+startPractice()
+
+function startPractice() {
+    gameActive = false
+    secret = false
+    roomsGrid.innerHTML = ""
     for ( let i = 0; i < 12; i++) {
         roomsGrid.innerHTML += 
         `
             <div class="room-box">
+                <div class="flipper unflipped">
+                <div class="flipper-front">
                 <img src="${Object.values(roomsObj)[i]}">
-                <div class="middle-dot"></div>
-                <div class="corner-dot"></div>
+                </div>
+                <div class="flipper-back">
+                </div>
+                </div>
             </div>
         `
     }
     entrance.innerHTML = `
     <img src="${roomsObj["entrance"]}">
-    <div class="corner-dot"></div>
     `
+    movers()
+}
+function startTreasure() {
+    gameActive = true
+    secret = false
+    roomsGrid.innerHTML = ""
+    for ( let i = 0; i < 12; i++) {
+        roomsGrid.innerHTML += 
+        `
+            <div class="room-box">
+                <div class="flipper unflipped">
+                <div class="flipper-front">
+                <img src="${Object.values(roomsObj)[i]}">
+                </div>
+                <div class="flipper-back">
+                </div>
+                </div>
+            </div>
+        `
+    }
+    let getRandom = Math.floor( Math.random()*12 )
+    let allBack = document.querySelectorAll(".flipper-back")
+    allBack[getRandom].innerHTML = `<img src="./images/commonitems/img20.png">`
+    console.log(getRandom)
+
+    entrance.innerHTML = `
+    <img src="${roomsObj["entrance"]}">
+    `
+    movers()
 }
 
+function startSecret() {
+    gameActive = true
+    secret = true
+    let roomsList = Object.values(roomsObj).slice(0,12)
+    roomsList.sort( ()=> {return 0.5 - Math.random() } )
+    roomsGrid.innerHTML = ""
+    for ( let i = 0; i < 12; i++) {
+        roomsGrid.innerHTML += 
+        `
+            <div class="room-box">
+                <div class="flipper unflipped">
+                <div class="flipper-front secret-mode">
+                <img src="${numbersArr[i]}">
+                </div>
+                <div class="flipper-back secret-mode">
+                <img src="${roomsList[i]}">
+                </div>
+                </div>
+            </div>
+        `
+    }
+    entrance.innerHTML = `
+    <img src="${roomsObj["entrance"]}">
+    `
+    movers()
+}
 
-
-let theMover = moverObj["fighterjet"]
-movers()
 function movers() {
+    moverGrid.innerHTML = ""
+    moverPosition = 38
     for ( let i = 0; i < 42; i++ ) {
         moverGrid.innerHTML += `
         <div class="mover-box"></div>
         `
     }
-    for ( let i = 0; i < 7; i++ ) {
+    for ( let i = 0; i < 35; i++ ) {
         moverGrid.children[i].classList.add("can-move")
+        if ( i === 6 ) {
+            i = 13
+        } else if ( i === 20 ) {
+            i = 27
+        }
     }
-    for ( let i = 14; i < 21; i++ ) {
-        moverGrid.children[i].classList.add("can-move")
-    }
-    for ( let i = 28; i < 35; i++ ) {
-        moverGrid.children[i].classList.add("can-move")
+    for ( let i = 0; i <= 35; i+=2 ) {
+        moverGrid.children[i].classList.add("entry")
+        if ( i === 6 ) {
+            i = 12
+        } else if ( i === 20 ) {
+            i = 26
+        }
     }
     moverGrid.children[38].innerHTML += `
     <img class="mover north" src="${theMover}">
@@ -117,7 +220,24 @@ function getMoverPos() {
         `
         moverPosition -= 7
     } else if ( mover.classList.contains("north") ) {
-        if ( moverGrid.children[moverPosition-14].classList.contains("can-move") ) {
+        if ( moverGrid.children[moverPosition].classList.contains("entry") ) {
+            let entryPoint = entryObj[moverPosition]
+            if ( gameActive ) {
+                roomsGrid.children[entryPoint].children[0].style.transform = "rotatex(180deg)"
+                if ( !secret ) {
+                setTimeout( ()=>{
+                    roomsGrid.children[entryPoint].children[0].style.transform = "rotatex(0deg)"
+                },2000)
+            }
+            } else {
+                roomsGrid.children[entryPoint].children[0].classList.add("highlight")
+                setTimeout( ()=>{
+                    roomsGrid.children[entryPoint].children[0].classList.remove("highlight")
+                },2000)
+
+            }
+
+        } else if ( moverGrid.children[moverPosition-14].classList.contains("can-move") ) {
             document.querySelector(".ishere").classList.remove("ishere")
             moverGrid.children[moverPosition-14].classList.add("ishere")
             moverGrid.children[moverPosition].innerHTML = ""
@@ -147,7 +267,15 @@ function getMoverPos() {
             moverPosition += 1
         }
     } else if ( mover.classList.contains("south") ) {
-        if ( moverGrid.children[moverPosition+14].classList.contains("can-move") ) {
+        if ( moverPosition === 31 ) {
+            document.querySelector(".ishere").classList.remove("ishere")
+            moverGrid.children[38].classList.add("ishere")
+            moverGrid.children[moverPosition].innerHTML = ""
+            moverGrid.children[38].innerHTML =`
+            <img class="mover south" src="${theMover}">
+            `
+            moverPosition = 38
+        } else if ( moverGrid.children[moverPosition+14].classList.contains("can-move") && !moverGrid.children[moverPosition].classList.contains("entry") ) {
             document.querySelector(".ishere").classList.remove("ishere")
             moverGrid.children[moverPosition+14].classList.add("ishere")
             moverGrid.children[moverPosition].innerHTML = ""
